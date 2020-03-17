94004ed8d7739369e8c557450b613095414c858e
Toni <toni.asikainen@icloud.com>
2020-03-17 20:33:39 +0200

    Added a test label just to see if this thing works

--- testing2/ViewController.swift
+++ testing2/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        testLabel.text = "SHIT!"
+        
     }
 
 
